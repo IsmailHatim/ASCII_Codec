2648a3b4f646872be2bd32381456d3dadc552426
IsmailHatim <ismoil59600@gmail.com>
2022-10-10 23:47:05 +0200

    Fixing issues

--- src/img2ascii.py
+++ src/img2ascii.py
@@ -7,18 +7,20 @@ ASCII_SHADERS = " .:s%@"
 
 
 
-
+def list_to_array(data : list) -> list:
+    return [[data[i] for i in range(k*width,(k+1)*width) ] for k in range(0, height)]
 
 class Sub_sampler:
+    """The main class that permit to sample a High resolution image into a lower resolution with given size of sub_sampling
+
+    """
+    sampled_mat = []
+
     def __init__(self, mat : list, size : int):
-        self.mat = self.list_to_array(mat)
+        self.mat = list_to_array(mat)
         self.size = size
-    
-    def list_to_array(data : list) -> list:
-        return [([data[i] for i in range(k*width,(k+1)*width) ]) for k in range(0, height)]
 
     def sample(self) -> Image.Image:
-        sampled_mat = []
         for h in range(0,len(self.mat),self.size):
             moy = 0
             line = []
@@ -27,9 +29,9 @@ class Sub_sampler:
                     for j in range(0,self.size):
                         moy += self.mat[h+i][k+j]
                 moy = moy/(self.size*self.size)
-                line.append(moy)
-            sampled_mat.append(line)
-        return Image.fromarray(np.array(sampled_mat))
+                line.append(int(moy))
+            self.sampled_mat.append(line)
+        return Image.fromarray(np.array(self.sampled_mat))
 
 with Image.open("data/img.jpg") as img:
     img_cropped_grey = ImageOps.fit(img.convert("L"),(img.size[0]-img.size[0]%SAMPLER_SIZE,img.size[1]-img.size[1]%SAMPLER_SIZE))
@@ -47,6 +49,8 @@ def main():
     sampled_img = sub_sampler.sample()
 
     sampled_img.show()
+
+    
 
 
 main()
