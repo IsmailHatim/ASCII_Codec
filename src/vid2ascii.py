7de56e5567e8cf7509ddc429b29ccad7258b56a8
IsmailHatim <ismoil59600@gmail.com>
2022-10-13 00:06:25 +0200

    Importing libraries & opening videos

---
+++ src/vid2ascii.py
@@ -0,0 +1,13 @@
+import img2ascii
+import cv2
+
+cap = cv2.VideoCapture("data/vid1.mp4")
+ret, frame = cap.read()
+while(1):
+   ret, frame = cap.read()
+   cv2.imshow('frame',frame)
+   if cv2.waitKey(1) & 0xFF == ord('q') or ret==False :
+       cap.release()
+       cv2.destroyAllWindows()
+       break
+   cv2.imshow('frame',frame)
